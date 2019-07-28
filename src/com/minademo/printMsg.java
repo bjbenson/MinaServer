ed9ffbb1e5aeda07a81b67083577588a0dc1758c
chuliangwu <chuliangwu@piesat.cn>
2019-07-28 19:50:25 +0800

    test

--- src/com/minademo/printMsg.java
+++ src/com/minademo/printMsg.java
@@ -7,6 +7,6 @@ package com.minademo;
 public class printMsg {
 
     public static void print() {
-        System.out.println("zapierTest11");
+        System.out.println("zapierTest123456");
     }
 }
